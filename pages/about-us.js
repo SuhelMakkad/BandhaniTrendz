0795074b8e714fe509173f49ef99effbc3f1d5bc
Suhel Makkad <MAKADSUHEL11@GMAIL.COM>
2022-03-01 20:56:15 +0530

    added about us page

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,9 +1,51 @@
+import { useState, useRef, useLayoutEffect } from "react";
+
 import Head from "next/head";
+import Image from "next/image";
+
 import Footer from "../component/Footer";
 import Navbar from "../component/Navbar";
-import styles from "../styles/Catagories.module.css";
+
+import styles from "../styles/AboutUs.module.css";
+import { LogoBox } from "../component/Icons";
 
 export default function AboutUs() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const descriptionInput = useRef(null);
+  const firstSection = useRef(null);
+
+  const handleDescriptionChange = (e) => {
+    const target = e.target;
+    setMessage(target.value);
+
+    target.style.height = "auto";
+    target.style.height = (target.scrollHeight < 100 ? 100 : target.scrollHeight + 10) + "px";
+  };
+
+  const handleSubmit = (e) => {
+    console.log(e);
+  };
+
+  useLayoutEffect(() => {
+    if (!(firstSection && firstSection.current)) return;
+
+    const sectionOneOptions = {
+      rootMargin: "0px 0px 0px 0px",
+      threshold: 1,
+    };
+
+    const sectionOneObserver = new IntersectionObserver((entries, sectionOneObserver) => {
+      entries.forEach((entry) => {
+        setIsIntersecting(entry.isIntersecting);
+      });
+    }, sectionOneOptions);
+
+    sectionOneObserver.observe(firstSection.current);
+
+    return () => sectionOneObserver && sectionOneObserver.unobserve(firstSection.current);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,10 +54,86 @@ export default function AboutUs() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar isTransparent={false} />
+      <Navbar isActive={isIntersecting} isTransparent={true} />
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <section
+          id={styles.heroSection}
+          className="bg-gradient"
+          ref={firstSection}
+          aria-describedby="BANDHANI TRENDZ is a manufacturer Of finest BANDHANI cloths"
+        >
+          <div className="wrapper">
+            <header>
+              <h2 className={styles.heading}>
+                BANDHANI TRENDZ is a manufacturer <br /> Of finest BANDHANI cloths
+              </h2>
+            </header>
 
+            <p className={styles.subHeading}>Founded by Uvesh Sadeki</p>
+          </div>
+        </section>
+
+        <section id={styles.about} aria-describedby="about usvesh">
+          <div className="wrapper">
+            <div className={styles.imageWrapper}>
+              <Image
+                className={styles.mainImage}
+                src="/assets/images/uvesh.jpg"
+                width={275}
+                height={275}
+                layout="intrinsic"
+              />
+            </div>
+            <h4 className={styles.heading}>Uvesh Sadeki</h4>
+            <p className={styles.subHeading}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua
+            </p>
+          </div>
+        </section>
+
+        <section
+          className="bg-gradient bg-pattern-1"
+          id={styles.decoration}
+          aria-describedby="decoration section"
+        >
+          <div className={`wrapper ${styles.logoWrapper}`}>
+            <LogoBox />
+          </div>
+        </section>
+
+        <section aria-describedby="Get in touch!" id={styles.contact}>
+          <div className="wrapper">
+            <header>
+              <h4 className={styles.heading}>Get in touch</h4>
+            </header>
+            <div>
+              <form className={styles.contactForm} onSubmit={handleSubmit}>
+                <input
+                  className={styles.formInputs}
+                  id={styles.emailInput}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your e-mail or phone number"
+                  type="email"
+                />
+                <textarea
+                  ref={descriptionInput}
+                  className={styles.formInputs}
+                  id={styles.descriptionInput}
+                  value={message}
+                  onChange={handleDescriptionChange}
+                  placeholder="Enter your message"
+                />
+                <button type="submit" id={styles.submitContactFormBtn}>
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </section>
+      </main>
       <Footer />
     </div>
   );
